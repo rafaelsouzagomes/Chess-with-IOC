d2f7df091a18704f91d9acd805dd63b6c8d459f9
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-05-23 21:33:58 -0300

    fix:improve methods to create the squares component of the chess

--- src/main/java/com/game/chess/components/ChessBoardFactory.java
+++ src/main/java/com/game/chess/components/ChessBoardFactory.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.game.chess.components.chessSquare.SquareBoard;
+import com.game.chess.enums.EnumNameNotaionSquare;
 import com.game.chess.enums.EnumTeam;
 import com.game.chess.enums.EnumTypePiece;
 
@@ -19,78 +20,80 @@ public class ChessBoardFactory implements GameBoardFactory {
 	}
 
 	private void createFields() {
-		boardFactory.addSquare_A8(EnumTypePiece.ROOK, EnumTeam.BLACK);
-	    boardFactory.addSquare_B8(EnumTypePiece.KNIGHT, EnumTeam.BLACK);
-	    boardFactory.addSquare_C8(EnumTypePiece.BISHOP, EnumTeam.BLACK);
-	    boardFactory.addSquare_D8(EnumTypePiece.QUEEN, EnumTeam.BLACK);
-	    boardFactory.addSquare_E8(EnumTypePiece.KING, EnumTeam.BLACK);
-	    boardFactory.addSquare_F8(EnumTypePiece.BISHOP, EnumTeam.BLACK);
-	    boardFactory.addSquare_G8(EnumTypePiece.KNIGHT, EnumTeam.BLACK);
-	    boardFactory.addSquare_H8(EnumTypePiece.ROOK, EnumTeam.BLACK);
+	    boardFactory.addSquare(EnumTypePiece.ROOK, EnumTeam.BLACK, EnumNameNotaionSquare.A8);
+	    boardFactory.addSquare(EnumTypePiece.KNIGHT, EnumTeam.BLACK, EnumNameNotaionSquare.B8);
+	    boardFactory.addSquare(EnumTypePiece.BISHOP, EnumTeam.BLACK, EnumNameNotaionSquare.C8);
+	    boardFactory.addSquare(EnumTypePiece.QUEEN, EnumTeam.BLACK, EnumNameNotaionSquare.D8);
+	    boardFactory.addSquare(EnumTypePiece.KING, EnumTeam.BLACK, EnumNameNotaionSquare.E8);
+	    boardFactory.addSquare(EnumTypePiece.BISHOP, EnumTeam.BLACK, EnumNameNotaionSquare.F8);
+	    boardFactory.addSquare(EnumTypePiece.KNIGHT, EnumTeam.BLACK, EnumNameNotaionSquare.G8);
+	    boardFactory.addSquare(EnumTypePiece.ROOK, EnumTeam.BLACK, EnumNameNotaionSquare.H8);
 
-	    boardFactory.addSquare_A7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_B7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_C7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_D7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_E7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_F7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_G7(EnumTypePiece.PAWN, EnumTeam.BLACK);
-	    boardFactory.addSquare_H7(EnumTypePiece.PAWN, EnumTeam.BLACK);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.A7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.B7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.C7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.D7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.E7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.F7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.G7);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.BLACK, EnumNameNotaionSquare.H7);
 
-	    boardFactory.addSquare_A6();
-	    boardFactory.addSquare_B6();
-	    boardFactory.addSquare_C6();
-	    boardFactory.addSquare_D6();
-	    boardFactory.addSquare_E6();
-	    boardFactory.addSquare_F6();
-	    boardFactory.addSquare_G6();
-	    boardFactory.addSquare_H6();
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.A6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.B6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.C6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.D6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.E6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.F6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.G6);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.H6);
 
-	    boardFactory.addSquare_A5();
-	    boardFactory.addSquare_B5();
-	    boardFactory.addSquare_C5();
-	    boardFactory.addSquare_D5();
-	    boardFactory.addSquare_E5();
-	    boardFactory.addSquare_F5();
-	    boardFactory.addSquare_G5();
-	    boardFactory.addSquare_H5();
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.A5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.B5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.C5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.D5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.E5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.F5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.G5);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.H5);
 
-	    boardFactory.addSquare_A4();
-	    boardFactory.addSquare_B4();
-	    boardFactory.addSquare_C4();
-	    boardFactory.addSquare_D4();
-	    boardFactory.addSquare_E4();
-	    boardFactory.addSquare_F4();
-	    boardFactory.addSquare_G4();
-	    boardFactory.addSquare_H4();
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.A4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.B4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.C4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.D4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.E4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.F4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.G4);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.H4);
 
-	    boardFactory.addSquare_A3();
-	    boardFactory.addSquare_B3();
-	    boardFactory.addSquare_C3();
-	    boardFactory.addSquare_D3();
-	    boardFactory.addSquare_E3();
-	    boardFactory.addSquare_F3();
-	    boardFactory.addSquare_G3();
-	    boardFactory.addSquare_H3();
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.A3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.B3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.C3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.D3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.E3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.F3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.G3);
+	    boardFactory.addSquare_empty(EnumNameNotaionSquare.H3);
 
-	    boardFactory.addSquare_A2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_B2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_C2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_D2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_E2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_F2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_G2(EnumTypePiece.PAWN, EnumTeam.WHITE);
-	    boardFactory.addSquare_H2(EnumTypePiece.PAWN, EnumTeam.WHITE);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.A2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.B2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.C2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.D2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.E2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.F2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.G2);
+	    boardFactory.addSquare(EnumTypePiece.PAWN, EnumTeam.WHITE, EnumNameNotaionSquare.H2);
 
-	    boardFactory.addSquare_A1(EnumTypePiece.ROOK, EnumTeam.WHITE);
-	    boardFactory.addSquare_B1(EnumTypePiece.KNIGHT, EnumTeam.WHITE);
-	    boardFactory.addSquare_C1(EnumTypePiece.BISHOP, EnumTeam.WHITE);
-	    boardFactory.addSquare_D1(EnumTypePiece.QUEEN, EnumTeam.WHITE);
-	    boardFactory.addSquare_E1(EnumTypePiece.KING, EnumTeam.WHITE);
-	    boardFactory.addSquare_F1(EnumTypePiece.BISHOP, EnumTeam.WHITE);
-	    boardFactory.addSquare_G1(EnumTypePiece.KNIGHT, EnumTeam.WHITE);
-	    boardFactory.addSquare_H1(EnumTypePiece.ROOK, EnumTeam.WHITE);
+	    boardFactory.addSquare(EnumTypePiece.ROOK, EnumTeam.WHITE, EnumNameNotaionSquare.A1);
+	    boardFactory.addSquare(EnumTypePiece.KNIGHT, EnumTeam.WHITE, EnumNameNotaionSquare.B1);
+	    boardFactory.addSquare(EnumTypePiece.BISHOP, EnumTeam.WHITE, EnumNameNotaionSquare.C1);
+	    boardFactory.addSquare(EnumTypePiece.QUEEN, EnumTeam.WHITE, EnumNameNotaionSquare.D1);
+	    boardFactory.addSquare(EnumTypePiece.KING, EnumTeam.WHITE, EnumNameNotaionSquare.E1);
+	    boardFactory.addSquare(EnumTypePiece.BISHOP, EnumTeam.WHITE, EnumNameNotaionSquare.F1);
+	    boardFactory.addSquare(EnumTypePiece.KNIGHT, EnumTeam.WHITE, EnumNameNotaionSquare.G1);
+	    boardFactory.addSquare(EnumTypePiece.ROOK, EnumTeam.WHITE, EnumNameNotaionSquare.H1);
 	}
+
+
 	
 	@Autowired
 	public void setBoardFactory(Board8x8Factory boardFactory) {
