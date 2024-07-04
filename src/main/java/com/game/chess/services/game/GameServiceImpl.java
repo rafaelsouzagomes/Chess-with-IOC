693842e967c7d3f918deb08d67453f95f25dafce
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-07-03 23:14:23 -0300

    add gameServiceimpl

---
+++ src/main/java/com/game/chess/services/game/GameServiceImpl.java
@@ -0,0 +1,13 @@
+package com.game.chess.services.game;
+
+import com.game.chess.services.GameService;
+
+public class GameServiceImpl implements GameService {
+
+	@Override
+	public void initGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
