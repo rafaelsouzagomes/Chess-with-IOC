eca09f06e55cc53878de649d79a92b223b42a91c
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-07-07 00:34:45 -0300

    add: postgre

--- src/main/java/com/game/chess/services/game/GameServiceImpl.java
+++ src/main/java/com/game/chess/services/game/GameServiceImpl.java
@@ -1,15 +1,23 @@
 package com.game.chess.services.game;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.game.chess.entities.Game;
+import com.game.chess.repositories.GameRepository;
 import com.game.chess.services.GameService;
 
 @Service
 public class GameServiceImpl implements GameService {
-
+	
+	@Autowired
+	GameRepository gameRepository;
+	
 	@Override
 	public void initGame() {
-		// TODO Auto-generated method stub
+		Game game = new Game();
+		game.setName("game1");
 		
+		gameRepository.save(game);
 	}
 }
