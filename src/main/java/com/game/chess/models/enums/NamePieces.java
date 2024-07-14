74c746580c60f0199852937fbfe22164a3805dfc
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-07-14 16:21:13 -0300

    add: static list to facilitate manipulate pieces names

--- src/main/java/com/game/chess/models/enums/NamePieces.java
+++ src/main/java/com/game/chess/models/enums/NamePieces.java
@@ -1,5 +1,8 @@
  package com.game.chess.models.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NamePieces {
 
 	public static final String ROOK= "Rook";
@@ -8,5 +11,20 @@ public class NamePieces {
 	public static final String BISHOP = "Bishop";
 	public static final String QUEEN = "Queen";
 	public static final String KING = "King";
+	
+	public static List<String> pieces;
+	
+	static {
+		pieces.add(ROOK);
+		pieces.add(PAWN);
+		pieces.add(KNIGHT);
+		pieces.add(BISHOP);
+		pieces.add(QUEEN);
+		pieces.add(KING);
+	}
+	
+	public static List<String> getPieces() {
+		return new ArrayList<>(pieces);
+	}
 	
 }
